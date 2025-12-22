1a6e62591ddb59ca52c910d9167a50158b886d5a
Aleksa Perić <alex.peric1205@gmail.com>
2025-12-22 23:59:16 +0100

    Finished fast api

--- mlaas/main.py
+++ mlaas/main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
 import joblib
 import numpy as np
+from pydantic import BaseModel
 
 model = joblib.load("./training/ml_model.pkl")
 app = FastAPI()
+
+class PredictRequest(BaseModel):
+    past_values: list[float]
+    
+class PredictResponse(BaseModel):
+    prediction: float
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the MLaaS API"}
 
 @app.post("/predict/")
-def predict(data: dict):
-    features = np.array(data["features"]).reshape(1, -1)
-    prediction = model.predict(features)
-    class_name = iris.target_names[prediction][0]
-    return {"class": class_name}
+def predict(request: PredictRequest) -> PredictResponse:
+    past_values = request.past_values
+    predicted_avg = model.predict([past_values])[0]
+    return {"prediction": predicted_avg}
+
+# to run this app run uvicorn main:app --reload
+# and to open swagger go to url/docs
